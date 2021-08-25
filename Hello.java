48e030b9181902f354b6b89c07d3ca7224d59cc1
TyreseWhitehead <89534997+TyreseWhitehead@users.noreply.github.com>
2021-08-25 11:34:26 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,24 @@
+/**************************************************************
+* Hello.java
+***************************************************************
+* Tyrese Whitehead
+* 8/25/21
+* CMSC 255
+**************************************************************/
+
+public class Hello {
+    public static void main(String[] args) {
+
+       System.out.println("Tyrese Whitehead");
+       System.out.println("Hello.java");
+       System.out.println("Lab 1");
+       System.out.println("8/25/21");
+	   System.out.println("CMSC 255 Section 004");
+	   System.out.println();
+	   
+	
+	
+	
+	
+	}
+}
